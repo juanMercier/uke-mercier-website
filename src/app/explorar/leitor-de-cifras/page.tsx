669ef03dea24ad0b8380525aee774dd4d510d08d
juanMercier <joaomercier@gmail.com>
2024-10-17 11:14:04 -0500

    Added full screen

--- src/app/explorar/leitor-de-cifras/page.tsx
+++ src/app/explorar/leitor-de-cifras/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect, useRef } from "react";
-import { Menu, Search, Minus, Plus, Play, Pause } from "lucide-react";
+import { Menu, Search, Minus, Plus, Play, Pause, Maximize, Minimize } from "lucide-react";
 import { initializeApp } from "firebase/app";
 import { getStorage, ref, listAll, getDownloadURL } from "firebase/storage";
 import dynamic from "next/dynamic";
@@ -29,6 +29,7 @@ export default function CifraReader() {
   const [filteredSongs, setFilteredSongs] = useState<{ name: string; url: string }[]>([]);
   const [isScrolling, setIsScrolling] = useState(false);
   const [wasManuallyScrolled, setWasManuallyScrolled] = useState(false);
+  const [isFullScreen, setIsFullScreen] = useState(false);
   const contentRef = useRef<HTMLDivElement>(null);
   const lastScrollTopRef = useRef(0);
 
@@ -81,7 +82,6 @@ export default function CifraReader() {
     }
   };
 
-
   const handleSongSelect = (song: { name: string; url: string }) => {
     setSelectedSong(song);
     setWasManuallyScrolled(false);
@@ -110,27 +110,31 @@ export default function CifraReader() {
     }
   };
 
+  const toggleFullScreen = () => {
+    setIsFullScreen(!isFullScreen);
+  };
+
   return (
-    <div className="pt-24 lg:pt-16">
+    <div className={`${isFullScreen ? 'fixed inset-0 z-[60] bg-white' : 'pt-24 lg:pt-16'}`}>
       <div className="flex h-screen bg-gray-100">
         {/* Sidebar */}
-        <div className={`${sidebarOpen ? "w-64" : "w-0"} transition-all duration-300 bg-white shadow-lg overflow-hidden`}>
-          <div className="p-4 h-full overflow-y-auto scrollbar-hide">
-            <h2 className="text-xl font-bold mb-4">Biblioteca de Cifras</h2>
-            <div className="relative mb-4">
-            <input
-              type="text"
-              placeholder="Buscar cifras..."
-              value={searchTerm}
-              onChange={handleInputChange}
-              className="w-full p-2 border rounded"
-            />
-            <Search className="absolute right-3 top-2.5 text-gray-400" size={20} />
-          </div>
-            <ul className="space-y-2">
-              {filteredSongs.length > 0 ? (
-                <ul className="space-y-2">
-                  {filteredSongs.map((song, index) => (
+        {!isFullScreen && (
+          <div className={`${sidebarOpen ? "w-64" : "w-0"} transition-all duration-300 bg-white shadow-lg overflow-hidden`}>
+            <div className="p-4 h-full overflow-y-auto scrollbar-hide">
+              <h2 className="text-xl font-bold mb-4">Biblioteca de Cifras</h2>
+              <div className="relative mb-4">
+                <input
+                  type="text"
+                  placeholder="Buscar cifras..."
+                  value={searchTerm}
+                  onChange={handleInputChange}
+                  className="w-full p-2 border rounded"
+                />
+                <Search className="absolute right-3 top-2.5 text-gray-400" size={20} />
+              </div>
+              <ul className="space-y-2">
+                {filteredSongs.length > 0 ? (
+                  filteredSongs.map((song, index) => (
                     <li key={index}>
                       <button
                         onClick={() => handleSongSelect(song)}
@@ -139,46 +143,51 @@ export default function CifraReader() {
                         {song.name}
                       </button>
                     </li>
-                  ))}
-                </ul>
-              ) : (
-                <p>Sem Resultados</p>
-              )}
-            </ul>
+                  ))
+                ) : (
+                  <p>Sem Resultados</p>
+                )}
+              </ul>
+            </div>
           </div>
-        </div>
+        )}
 
         {/* Main content */}
         <div className="flex-1 overflow-hidden bg-primary">
-          <div className="flex justify-between items-center p-4 bg-white shadow">
-            <button onClick={() => setSidebarOpen(!sidebarOpen)} className="p-2 rounded hover:bg-gray-100">
-              <Menu size={24} />
-              <span className="sr-only">Toggle sidebar</span>
-            </button>
-            <div className="flex items-center space-x-4">
-              <button onClick={() => setScrollSpeed(Math.max(0, scrollSpeed - 0.5))} className="p-2 rounded hover:bg-gray-100">
-                <Minus size={20} />
-                <span className="sr-only">Decrease scroll speed</span>
+          {!isFullScreen && (
+            <div className="flex justify-between items-center p-4 bg-white shadow">
+              <button onClick={() => setSidebarOpen(!sidebarOpen)} className="p-2 rounded hover:bg-gray-100">
+                <Menu size={24} />
+                <span className="sr-only">Toggle sidebar</span>
               </button>
-              <span className="font-bold">{scrollSpeed.toFixed(1)}</span>
-              <button onClick={() => setScrollSpeed(scrollSpeed + 0.5)} className="p-2 rounded hover:bg-gray-100">
-                <Plus size={20} />
-                <span className="sr-only">Increase scroll speed</span>
-              </button>
-              <button onClick={toggleScrolling} className="p-2 rounded hover:bg-gray-100">
-                {isScrolling ? <Pause size={20} /> : <Play size={20} />}
-                <span className="sr-only">{isScrolling ? 'Pause scrolling' : 'Start scrolling'}</span>
-              </button>
+              <div className="flex items-center space-x-4">
+                <button onClick={() => setScrollSpeed(Math.max(0, scrollSpeed - 0.5))} className="p-2 rounded hover:bg-gray-100">
+                  <Minus size={20} />
+                  <span className="sr-only">Decrease scroll speed</span>
+                </button>
+                <span className="font-bold">{scrollSpeed.toFixed(1)}</span>
+                <button onClick={() => setScrollSpeed(scrollSpeed + 0.5)} className="p-2 rounded hover:bg-gray-100">
+                  <Plus size={20} />
+                  <span className="sr-only">Increase scroll speed</span>
+                </button>
+                <button onClick={toggleScrolling} className="p-2 rounded hover:bg-gray-100">
+                  {isScrolling ? <Pause size={20} /> : <Play size={20} />}
+                  <span className="sr-only">{isScrolling ? 'Pause scrolling' : 'Start scrolling'}</span>
+                </button>
+                <button onClick={toggleFullScreen} className="p-2 rounded hover:bg-gray-100">
+                  <Maximize size={20} />
+                  <span className="sr-only">Enter full screen</span>
+                </button>
+              </div>
             </div>
-          </div>
+          )}
           <div
             ref={contentRef}
-            className="h-full overflow-y-auto p-8 scrollbar-hide"
+            className={`h-full overflow-y-auto p-8 scrollbar-hide ${isFullScreen ? 'h-screen' : ''}`}
             onScroll={handleScroll}
           >
             {selectedSong ? (
               <div>
-                {/* <h2 className="text-2xl font-bold mb-4">{selectedSong.name}</h2> */}
                 <PDFRenderer url={selectedSong.url} />
               </div>
             ) : (
@@ -190,6 +199,25 @@ export default function CifraReader() {
           </div>
         </div>
       </div>
+      {isFullScreen && (
+        <div className="fixed top-4 right-4 z-[70] bg-white rounded-lg shadow-md p-2">
+          <div className="flex items-center space-x-4">
+            <button onClick={() => setScrollSpeed(Math.max(0, scrollSpeed - 0.5))} className="p-2 rounded hover:bg-gray-100">
+              <Minus size={20} />
+            </button>
+            <span className="font-bold">{scrollSpeed.toFixed(1)}</span>
+            <button onClick={() => setScrollSpeed(scrollSpeed + 0.5)} className="p-2 rounded hover:bg-gray-100">
+              <Plus size={20} />
+            </button>
+            <button onClick={toggleScrolling} className="p-2 rounded hover:bg-gray-100">
+              {isScrolling ? <Pause size={20} /> : <Play size={20} />}
+            </button>
+            <button onClick={toggleFullScreen} className="p-2 rounded hover:bg-gray-100">
+              <Minimize size={20} />
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
